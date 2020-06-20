26d2823d68e40d00a28ce0262248d21ed455d00a
Paulo Eduardo Correa <paulo.ec@hotmail.com>
2020-06-21 02:08:05 +0700

    Update RequestManager.swift

--- Pomelo/Service/Network/Request/RequestManager.swift
+++ Pomelo/Service/Network/Request/RequestManager.swift
@@ -38,11 +38,3 @@ class RequestManager {
         }
     }
 }
-
-
-extension String {
-    func toJSON() -> Any? {
-        guard let data = self.data(using: .utf8, allowLossyConversion: false) else { return nil }
-        return try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-    }
-}
